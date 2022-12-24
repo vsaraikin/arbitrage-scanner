a3bab9560c6c54fe7632fc44b0908ca43ec4882f
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-12-24 12:49:59 +0300

    multithreading attempt for ccxt

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from ticker_engineering.tools import read_data
 from scanner import exec_scanner
 import asyncio
+# from multiprocessing import Pool
+import concurrent.futures
+import time
+import random
 
-tickers_exchanges = read_data('ticker_engineering\exchanges_tickers.json')
+tickers_exchanges = read_data('ticker_engineering/exchanges_tickers.json')
 
-tickers = ['LTC/BTC']
-ex_list = tickers_exchanges['LTC/BTC']
+tickers = list(tickers_exchanges.keys())[20:30]
+ex_list = [tickers_exchanges[t] for t in tickers]
 
-asyncio.run(exec_scanner(tickers, ex_list))
+threshold = 1/1000
+
+def run_search(pair):
+    ticker, exchanges, threshold = pair
+    # time.sleep(random.randint(1, 10))
+    asyncio.run(exec_scanner(ticker, exchanges, threshold))
+
+pairs = [([ti], ex, threshold) for ti, ex in zip(tickers, ex_list)]
+
+if __name__ == '__main__':
+    # with Pool(processes=len(tickers)) as pool:
+    #     pool.starmap(run_search, pairs)
+    
+    with concurrent.futures.ThreadPoolExecutor(len(tickers)) as pool:
+        futures = [pool.submit(run_search, pair) for pair in pairs]
+        for f in futures:
+            f.result()
--- ticker_engineering/tools.py
+++ ticker_engineering/tools.py
@@ -4,8 +4,13 @@ import time
 import requests as r
 import pandas as pd
 from random import choice
+from termcolor import cprint
+import asyncio
+import aiohttp
+from multiprocessing import Pool
+from aiohttp_socks import ProxyConnector
 
-
+    
 def write_data(data, filename):
     with open(filename, "w") as file:
         json.dump(data, file)
@@ -31,10 +36,73 @@ def timeit(func):
     return timeit_wrapper
 
 
-def get_proxy(ssl):
-    res = r.get('https://free-proxy-list.net/', headers={'User-Agent':'Mozilla/5.0'}).text
+def get_proxy(ssl, url):
+    res = r.get(url, headers={'User-Agent':'Mozilla/5.0'}).text
     table = pd.read_html(res)[0]
+    table['IP'] = table['IP Address'].apply(str) + ':' +  table['Port'].apply(str)
+    cprint('Requested more proxies', 'yellow')
     if ssl:
-        return choice(table[table['Https'] == 'yes']['IP Address'].to_list())
+        return table[table['Https'] == 'yes']['IP'].to_list()
     else:
-        return choice(table[table['Https'] == 'no']['IP Address'].to_list())
+        return table[table['Https'] == 'no']['IP'].to_list()
+
+    
+
+@timeit
+def generate_proxies():
+    
+    timeout = 2
+    
+    async def check(socket_address: str, proto: str) -> None:
+        proxy_url = f"{proto}://{socket_address}"
+        async with ProxyConnector.from_url(proxy_url) as connector:
+            async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
+                try:
+                    response = await session.get("http://ip-api.com/json/?fields=8217",timeout=timeout,raise_for_status=True,)
+                    data = await response.json()
+                    await session.close()
+                    geolocation = "|{}|{}|{}".format(
+                        data["country"], data["regionName"], data["city"]
+                    )
+                    cprint(geolocation, 'yellow')
+                    return socket_address
+                
+                except Exception as e:
+                    await session.close()
+                    # print(e)
+
+    async def generate_with_proto(proto: str) -> str:
+
+            if proto == 'https':
+                while True:
+                    https_proxy_list = get_proxy(ssl=True, url='https://www.sslproxies.org/')
+                    tasks = [asyncio.ensure_future(check(proxy_ip, 'http')) for proxy_ip in https_proxy_list]
+                    
+                    for coro in asyncio.as_completed(tasks):
+                        result = await coro
+                        if isinstance(result, str):
+                            del tasks # the same as loop.stop() but without errors
+                            return result
+                    
+            elif proto == 'http':
+                while True:
+                    http_proxy_list = get_proxy(ssl=False, url='https://free-proxy-list.net/')
+                    tasks = [asyncio.ensure_future(check(proxy_ip, 'http')) for proxy_ip in http_proxy_list]
+                    
+                    for coro in asyncio.as_completed(tasks):
+                        result = await coro
+                        if isinstance(result, str):
+                            del tasks # the same as loop.stop() but without errors
+                            return result
+                        
+            else:
+                raise ValueError("Unknown proto type")
+
+    http_proxy = asyncio.run(generate_with_proto('http'))
+    https_proxy = asyncio.run(generate_with_proto('https'))
+    
+    if http_proxy and https_proxy:
+        return (http_proxy, https_proxy)
+    else:
+        exit()
+    
