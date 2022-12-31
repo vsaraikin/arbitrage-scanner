a7ec6e316706067ee11bd74429acde284656d059
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2022-12-31 11:42:32 +0300

    minor improvements

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import asyncio
 import ccxt.pro
-from termcolor import cprint
-import random
 from ticker_engineering.tools import timeit, generate_proxies, read_data
 from ticker_engineering.create_configs import ticker_ex_configs
 import logging
 import time
-import sys
 
 logging.basicConfig(filename=f'messages_{time.time()}.log',
                     filemode='a',
@@ -17,24 +14,20 @@ logging.basicConfig(filename=f'messages_{time.time()}.log',
 logger=logging.getLogger() 
 logger.setLevel(logging.INFO) 
 
-orderbooks = {}
 
 @timeit
 def handle_all_orderbooks(orderbooks):
     """
     Calculate arbitrage opportunity as: `exchange[ticker] with the lowest ask -- exchange[ticker] with the highest bid`
     """
-    market_data_tmp_ask = {symbol:{ex:float('inf') for ex in tickers_ex[symbol]} for symbol in tickers_ex}
-    market_data_tmp_bid = {symbol:{ex:float('-inf') for ex in tickers_ex[symbol]} for symbol in tickers_ex}
-        
+    
     for exchange_id, orderbooks_by_symbol in orderbooks.items():
         for symbol in orderbooks_by_symbol.keys():
             orderbook = orderbooks_by_symbol[symbol]
             if orderbook['bids'] and orderbook['asks']:
                 market_data_tmp_bid[symbol][exchange_id] = orderbook['bids'][0][0]
                 market_data_tmp_ask[symbol][exchange_id] = orderbook['asks'][0][0]
-                # tickers_ex.pop(symbol)
-            # print(ccxt.pro.Exchange.iso8601(orderbook['timestamp']), exchange_id, symbol, orderbook['bids'][0][0], orderbook['asks'][0][0],)
+                
     
     for symbol in tickers_ex.keys():
         min_key = min(market_data_tmp_ask[symbol], key=market_data_tmp_ask[symbol].get)
@@ -48,34 +41,33 @@ def handle_all_orderbooks(orderbooks):
             logger.info(f"{symbol}: BID {market_data_tmp_bid[symbol][max_key]} on {max_key} | ASK {market_data_tmp_ask[symbol][min_key]} on {min_key} // DIFF on {symbol}: {round(difference, 5)} or {round(differrence_pct * 100, 3)}%")
 
 
-async def symbol_loop(exchange: ccxt.pro.Exchange, symbol: str, id: int):
+async def symbol_loop(exchange: ccxt.pro.Exchange, symbol: str):
     """
     Checking whether a desired `exchange[ticker]` is liquid and listening to ordebook
     """
     while True:
-        # logger.info([exchange, symbol, id])
-        try:
-            ticker_data = await exchange.watch_ticker(symbol)
-            if ticker_data.get('baseVolume'): # and ticker.get('quoteVolume'): # ticker.get('bidVolume') and ticker.get('askVolume') and 
+        try:          
+            # ticker_data = await exchange.watch_ticker(symbol)
+            # if ticker_data.get('baseVolume'): # and ticker.get('quoteVolume'): # ticker.get('bidVolume') and ticker.get('askVolume') and 
                 orderbook = await exchange.watchOrderBook(symbol)
                 orderbooks[exchange.id] = orderbooks.get(exchange.id, {})
                 orderbooks[exchange.id][symbol] = orderbook
-                handle_all_orderbooks(orderbooks)
+                # handle_all_orderbooks(orderbooks)
                 
                 if (exchange, symbol) not in count_connection:
                     count_connection.add((exchange, symbol))
                     print(f"{round( 100 * (len(count_connection)/all_connections), 2)}%")
-                else:
-                    asyncio.sleep(0.000000000001)
-            else:
-                logger.warning(f"{symbol} on {exchange} is not liquid -> exit market")
-                break    
+                    await asyncio.sleep(0.000001)
+
+            # else:
+            #     logger.warning(f"{symbol} on {exchange} is not liquid -> exit market")
+            #     break    
             
         except ccxt.RequestTimeout as e:
             logger.critical(f"{type(e).__name__} {str(e)} on {exchange}") # recoverable error, do nothing and retry later
         except ccxt.DDoSProtection as e:
             logger.critical(f"{type(e).__name__} {str(e)} on {exchange}") # recoverable error, you might want to sleep a bit here and retry later
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.000001)
         except ccxt.ExchangeNotAvailable as e:
             logger.critical(f"{type(e).__name__} {str(e)} on {exchange}") # recoverable error, do nothing and retry later
         except ccxt.NetworkError as e:
@@ -91,7 +83,9 @@ async def spot_check(exchange: ccxt.pro.Exchange, symbols: list) -> list:
     """
     markets = await exchange.load_markets()
     new_symbols = []
+    c = 0
     for symbol in symbols:
+        c += 1
         res = markets.get(symbol)
 
         if not res:
@@ -105,7 +99,10 @@ async def spot_check(exchange: ccxt.pro.Exchange, symbols: list) -> list:
         if res.lower() != 'spot':
             print(symbol, exchange, 'is not spot but:', res)
         else:
-            new_symbols.append(symbol)
+            print(100 * round(c/len(symbols), 2), exchange)
+            ticker_data = await exchange.watch_ticker(symbol)
+            if ticker_data.get('baseVolume'): # and ticker.get('quoteVolume'): # ticker.get('bidVolume') and ticker.get('askVolume') and 
+                new_symbols.append(symbol)
             
     return new_symbols
    
@@ -113,17 +110,18 @@ async def exchange_loop(exchange_id: ccxt.pro.Exchange, symbols: list):
     
     exchange = getattr(ccxt.pro, exchange_id)(
         {
-        # 'http': proxy_http,
-        # 'https': proxy_https,
-        # 'aiohttp_proxy': proxy_http,
-        # 'enableRateLimit': True
+        # 'http': 'http://' + proxy_http,
+        # 'https': 'https://' + proxy_https,
+        # 'aiohttp_proxy': 'http://' + proxy_http,
+        # 'enableRateLimit': True,
+        # 'verify': False,
         }
     )
-    
+    # exchange.session.verify= False        # Do not reject on SSL certificate checks
     symbols = await spot_check(exchange, symbols)
     
     if len(symbols) > 0:
-        loops = [symbol_loop(exchange, symbol, random.randint(0, 10000)) for symbol in symbols]
+        loops = [symbol_loop(exchange, symbol) for symbol in symbols]
         await asyncio.gather(*loops)
         await exchange.close()
     else:
@@ -132,7 +130,8 @@ async def exchange_loop(exchange_id: ccxt.pro.Exchange, symbols: list):
 
 
 async def exec_scanner(exchanges: dict):
-    print("Total future connections:", len(exchanges.keys()))
+    print("Total future exchanges:", len(exchanges.keys()))
+    print("Total future subscribtions:", all_connections)
     loops = [exchange_loop(exchange_id, symbols) for exchange_id, symbols in exchanges.items()]
     await asyncio.gather(*loops)
 
@@ -141,8 +140,11 @@ async def exec_scanner(exchanges: dict):
 # print('Got proxy:', proxy_http, proxy_https)
 
 threshold = 1/100
-ticker_set = set(read_data('ticker_engineering/tickers_exchanges.json')['huobi'][:200])
+ticker_set = set(read_data('ticker_engineering/tickers_exchanges.json')['cryptocom'][:100])
 
 count_connection = set()
 ex_tickers, tickers_ex, all_connections = ticker_ex_configs(ticker_set)
+orderbooks = {}
+market_data_tmp_ask = {symbol:{ex:float('inf') for ex in tickers_ex[symbol]} for symbol in tickers_ex}
+market_data_tmp_bid = {symbol:{ex:float('-inf') for ex in tickers_ex[symbol]} for symbol in tickers_ex}
 asyncio.run(exec_scanner(ex_tickers))
