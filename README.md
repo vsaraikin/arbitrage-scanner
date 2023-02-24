0ee8cf6038a123715861b8a85edaf46fc1ca96df
Vladimir Saraikin <saraikinvladimir228boss@gmail.com>
2023-02-24 13:03:22 +0100

    minor improvemenets

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Install requirements:
 Run scanner:
 
 `python main.py`
+
+
+## How does scanner works?
+
+1. Scrape all possible pairs in `tickers_exchanges.json`.
+2. Match tickers with selected exchanges in `create_configs.py`.
+3. Check if market is liquid.
+   1. Create a DB to store checked pairs and attach a bool variable whether it is checked.
+   2. There is a baseVolume key in watch_ticker method which we assign to bool variable. True if baseVolume 350k+
+4. 
--- ticker_engineering/filtering_tickers.py
+++ ticker_engineering/filtering_tickers.py
@@ -10,6 +10,10 @@ import time
 
 FILTERED_CONFIGS = 'ticker_engineering/filtered_tickers_exchanges.json'
 
+class NoResponseFromExchange(Exception):
+    def __init__(self, message="No response from exchange: ") -> None:
+        super().__init__(message)
+
 
 async def spot_check(exchange: ccxt.pro.Exchange, symbol: str, markets: dict) -> str:
     """ Check whether a market is liquid """
@@ -41,16 +45,23 @@ async def prepare_symbols(exchange: ccxt.pro.Exchange, symbols: list) -> list:
 
 async def exchange_loop(exchange_id: ccxt.pro.Exchange, symbols: list):
     exchange = getattr(ccxt.pro, exchange_id)()
-    new_symbols = await prepare_symbols(exchange, symbols)
-    await exchange.close()
-    return {exchange_id: new_symbols}
-
+    try:
+        with timeout(5, exception=NoResponseFromExchange):
+            new_symbols = await prepare_symbols(exchange, symbols)
+            await exchange.close()
+            return {exchange_id: new_symbols}
+    except NoResponseFromExchange:
+        await exchange.close()
+        return
 
 async def run_filtering(exchanges: dict):
     # print("Total future exchanges:", len(exchanges.keys()))
     # print("Total future subscribtions:", all_connections)
     loops = [exchange_loop(exchange_id, symbols) for exchange_id, symbols in exchanges.items()]
     new_pairs = await tqdm.asyncio.tqdm.gather(*loops)
+    
+    # while new_pairs
+    
     merged_pairs = {key:val for d in new_pairs for key,val in d.items()}
     return merged_pairs
 
@@ -74,24 +85,14 @@ async def write_filtered_symbols(ex_tickers):
 top_tickers = read_data('ticker_engineering/all_tickers_exchanges.json')['binance']
 
 
-import multiprocessing
+from interruptingcow import timeout
 
-if __name__ == '__main__':
-    for _ in tqdm.tqdm(top_tickers):
-        ticker = random.choice(top_tickers)
-        ex_tickers, tickers_ex, all_connections = ticker_ex_configs({ticker})
-            
-        p = multiprocessing.Process(target=asyncio.run(write_filtered_symbols(ex_tickers)))
-        p.start()
-        p.join(10)
-        
-        if p.is_alive():
-            print("running... let's kill it...")
 
-            # Terminate - may not work if process is stuck for good
-            p.terminate()
-            # OR Kill - will work for sure, no chance for process to finish nicely however
-            # p.kill()
+# for _ in tqdm.tqdm(top_tickers):
+# ticker = random.choice(top_tickers)
+# ex_tickers, _, _ = ticker_ex_configs({ticker})
+# asyncio.run(write_filtered_symbols(ex_tickers))
 
-            p.join()
-        top_tickers.remove(ticker)
+# except NoResponseFromExchange:
+#     print ("didn't finish within 5 seconds")
+        # top_tickers.remove(ticker)
